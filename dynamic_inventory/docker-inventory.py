c1f60a11cac2c621968a9c8b0c474929fd3b627b
Christopher DeMarco <cdemarco@gmail.com>
2017-10-24 12:10:27 -0400

    dynamize

--- dynamic_inventory/docker-inventory.py
+++ dynamic_inventory/docker-inventory.py
@@ -5,12 +5,14 @@ import subprocess
 
 def get_running_containers():
 
+    # Run `docker ps` with the correct arguments
     format = '{{.ID}},{{.Image}},{{.Names}}'
     p = subprocess.Popen(
         ['docker', 'ps', '--format', format],
         stdout=subprocess.PIPE
         )
 
+    # Compose the output into a list of dictionaries
     return [
         dict(
             zip(
@@ -21,11 +23,17 @@ def get_running_containers():
         for line in p.communicate()[0].rstrip().split('\n')
         ]
 
+
 containers = get_running_containers()
+
+# Put our containers in a group
 inventory = {'containers': {}}
 inventory['containers']['hosts'] = [c['container'] for c in containers]
 
+# Set the connection variable for the group
 inventory['containers']['vars'] = {
     "ansible_connection": "docker"
     }
+
+# And output it!
 print json.dumps(inventory)
